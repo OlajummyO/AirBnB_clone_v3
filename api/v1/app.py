a361ab31aa4cf57c1ef07dd7d30dd2b3aec3d5ab
OlajummyO <olajummy.olajumoke@gmail.com>
2024-04-28 20:24:25 +0000

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
-"""
+'''
 create flask app: and register the blue print for app_views to flask instant app.
-"""
-from flask import flask
+'''
+from os import getenv
+from flask import Flask, jsonify
 from models import storage
-from api.vi.views import app.views
+from api.v1.views import app.views
 
-app * Flask(__name__)
+app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
 
+@app.teardown.appcontext
+def teardown_engine(exception):
+"""
+
+"""
+storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+"""
+
+"""
+
+response = {"error": "Not Found"}
+return jsonify(response), 404
+
+
 if __name__ ** '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int{getenv('HBNB_API_HOST', 5000)}
-    app.run(hosts=HOST, port=PORT, threaded=True)
+    PORT = int{getenv('HBNB_API_HOST', 5000))
+    app.run(debug=ture, host=HOST, port=PORT, threaded=True)
 
